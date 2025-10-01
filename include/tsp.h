cf8d3d4b16fd50c5bdee461c51cfcae93e829800
AlixRegnier <alix.regnier@inria.fr>
2025-10-01 14:05:53 +0200

    Naive NN

--- include/tsp.h
+++ include/tsp.h
@@ -14,9 +14,6 @@ namespace bms
         double distance;
     } typedef IndexDistance;
 
-    //Build a path by iteratively pushing back closest vertex (from tail)
-    std::size_t build_NN(const char* const MATRIX, DistanceMatrix& distanceMatrix, const std::size_t SUBSAMPLED_ROWS, const std::size_t OFFSET, std::vector<std::uint64_t>& order);
-
     //Build a path by iteratively add closest vertex (compare closest from tail and closest from head), less sensitive to the first chosen vertex
     std::size_t build_double_ended_NN(const char* const MATRIX, DistanceMatrix& distanceMatrix, const std::size_t SUBSAMPLED_ROWS, const std::size_t OFFSET, std::vector<std::uint64_t>& order);
 
@@ -24,7 +21,7 @@ namespace bms
     double columns_hamming_distance(const char* const MATRIX, const std::size_t NB_ROWS, const std::size_t COLUMN_A, const std::size_t COLUMN_B);
 
     //Get Nearest-Neighbor using VPTree
-    IndexDistance find_closest_vertex(VPTree<std::uint64_t>& VPTREE, const std::size_t VERTEX, const std::vector<bool>& ALREADY_ADDED);
+    IndexDistance find_closest_vertex(const DistanceMatrix& DISTANCE_MATRIX, const std::size_t VERTEX, const std::vector<bool>& ALREADY_ADDED);
 
     //Hamming distance between two buffers
     std::size_t hamming_distance(const char* const BUFFER1, const char* const BUFFER2, const std::size_t LENGTH);
--- src/tsp.cpp
+++ src/tsp.cpp
@@ -6,60 +6,8 @@
 #include <emmintrin.h>
 #include <stdint.h>
 
-namespace bms {
-
-    //TSP path, returns the number of computed distances
-    std::size_t build_NN(const char* const MATRIX, DistanceMatrix& distanceMatrix, const std::size_t SUBSAMPLED_ROWS, const std::size_t OFFSET, std::vector<std::uint64_t>& order)
-    {
-        //Pick a random first vertex
-        std::uint64_t firstVertex = RNG::rand_uint32_t(0, distanceMatrix.width());
-        
-        //Vector of added vertices (set true for the first vertex)
-        std::vector<bool> alreadyAdded;
-        alreadyAdded.resize(distanceMatrix.width());
-        alreadyAdded[firstVertex] = true;
-
-        //Deque for building path with first vertex as starting point
-        std::vector<std::uint64_t> path = {firstVertex};
-        path.reserve(distanceMatrix.width());
-
-        //Build vector of indices for VPTree
-        std::vector<std::uint64_t> vertices;
-        vertices.resize(distanceMatrix.width());
-        for(std::size_t i = 0; i < vertices.size(); ++i)
-            vertices[i] = i;
-
-        std::size_t counter = 0;
-
-        DistanceFunctions df = VPTree<std::uint64_t>::bind_distance_functions(
-            [=, &counter](std::uint64_t a, std::uint64_t b) -> double {
-                ++counter;
-                return columns_hamming_distance(MATRIX, SUBSAMPLED_ROWS, a+OFFSET, b+OFFSET);
-            },
-            [&distanceMatrix](std::uint64_t a, std::uint64_t b) -> double { return distanceMatrix.get(a, b); },
-            [&distanceMatrix](std::uint64_t a, std::uint64_t b, double d) { distanceMatrix.set(a, b, d); }
-        );
-
-        VPTree<std::uint64_t> root(vertices, &df);
-
-        //Added second vertex to data structures
-        //Find next vertices to add by checking which is the minimum to take
-        for(std::size_t i = 1; i < distanceMatrix.width(); ++i)
-        {
-            IndexDistance match = find_closest_vertex(root, path[i-1], alreadyAdded);
-            alreadyAdded[match.index] = true;
-            path.push_back(match.index);
-        }
-
-        //std::cout << "\tComputed distances (VPTree): " << counter << "/" << (distanceMatrix.width() * (distanceMatrix.width() - 1) / 2) <<  std::endl;
-
-        //Store global order
-        for(std::size_t i = 0; i < distanceMatrix.width(); ++i)
-            order[i+OFFSET] = path[i] + OFFSET; //Add offset because columns are addressed by their global location
-    
-        return counter;
-    }
-
+namespace bms 
+{
 
     //TSP path filled by both ends, less sensitive of the first chosen vertex, returns the number of computed distances
     std::size_t build_double_ended_NN(const char* const MATRIX, DistanceMatrix& distanceMatrix, const std::size_t SUBSAMPLED_ROWS, const std::size_t OFFSET, std::vector<std::uint64_t>& order)
@@ -81,30 +29,24 @@ namespace bms {
         for(std::size_t i = 0; i < vertices.size(); ++i)
             vertices[i] = i;
 
-        std::size_t counter = 0;
-
-        //Use counter to count how many distance computation could be avoided by using a VPTree
-        DistanceFunctions df = VPTree<std::uint64_t>::bind_distance_functions(
-            [=, &counter](std::uint64_t a, std::uint64_t b) -> double {
-                ++counter;
-                return columns_hamming_distance(MATRIX, SUBSAMPLED_ROWS, a+OFFSET, b+OFFSET);
-            },
-            [&distanceMatrix](std::uint64_t a, std::uint64_t b) -> double { return distanceMatrix.get(a, b); },
-            [&distanceMatrix](std::uint64_t a, std::uint64_t b, double d) { distanceMatrix.set(a, b, d); }
-        );
-
-        VPTree<std::uint64_t> root(vertices, &df);
+        for(std::size_t i = 0; i < distanceMatrix.width(); ++i)
+        {
+            for(std::size_t j = i+1; j < distanceMatrix.width(); ++j)
+            {
+                distanceMatrix.set(i, j, columns_hamming_distance(MATRIX, SUBSAMPLED_ROWS, i+OFFSET, j+OFFSET));
+            }
+        }
 
         //Find second vertex
-        IndexDistance second = find_closest_vertex(root, firstVertex, alreadyAdded);
+        IndexDistance second = find_closest_vertex(distanceMatrix, firstVertex, alreadyAdded);
         
         //Added second vertex to data structures
         orderDeque.push_back(second.index);
         alreadyAdded[second.index] = true;
 
         //Find closest vertices from path front and back
-        IndexDistance a = find_closest_vertex(root, orderDeque.front(), alreadyAdded);
-        IndexDistance b = find_closest_vertex(root, orderDeque.back(), alreadyAdded);
+        IndexDistance a = find_closest_vertex(distanceMatrix, orderDeque.front(), alreadyAdded);
+        IndexDistance b = find_closest_vertex(distanceMatrix, orderDeque.back(), alreadyAdded);
 
         //Find next vertices to add by checking which is the minimum to take
         for(std::size_t i = 2; i < distanceMatrix.width(); ++i)
@@ -115,9 +57,9 @@ namespace bms {
                 alreadyAdded[a.index] = true;
 
                 if(a.index == b.index)
-                    b = find_closest_vertex(root, orderDeque.back(), alreadyAdded);
+                    b = find_closest_vertex(distanceMatrix, orderDeque.back(), alreadyAdded);
 
-                a = find_closest_vertex(root, orderDeque.front(), alreadyAdded);
+                a = find_closest_vertex(distanceMatrix, orderDeque.front(), alreadyAdded);
             }
             else
             {
@@ -125,9 +67,9 @@ namespace bms {
                 alreadyAdded[b.index] = true;
 
                 if(b.index == a.index)
-                    a = find_closest_vertex(root, orderDeque.front(), alreadyAdded);
+                    a = find_closest_vertex(distanceMatrix, orderDeque.front(), alreadyAdded);
 
-                b = find_closest_vertex(root, orderDeque.back(), alreadyAdded);
+                b = find_closest_vertex(distanceMatrix, orderDeque.back(), alreadyAdded);
             }
         }
 
@@ -137,13 +79,20 @@ namespace bms {
         for(std::size_t i = 0; i < distanceMatrix.width(); ++i)
             order[i+OFFSET] = orderDeque[i] + OFFSET; //Add offset because columns are addressed by their global location
         
-        return counter;
+        return distanceMatrix.width() * (distanceMatrix.width() - 1) / 2;
     }
 
-    IndexDistance find_closest_vertex(VPTree<std::uint64_t>& VPTREE, const std::uint64_t VERTEX, const std::vector<bool>& ALREADY_ADDED)
+    IndexDistance find_closest_vertex(const DistanceMatrix& DISTANCE_MATRIX, const std::uint64_t VERTEX, const std::vector<bool>& ALREADY_ADDED)
     {
         IndexDistance nn = {0, 2.0};
-        VPTREE.get_unvisited_nearest_neighbor(VERTEX, ALREADY_ADDED, &nn.distance, &nn.index);
+        for(std::size_t i = 0; i < DISTANCE_MATRIX.width(); ++i)
+        {
+            if(!ALREADY_ADDED[i] && DISTANCE_MATRIX.get(i, VERTEX) < nn.distance)
+            {
+                nn.distance = DISTANCE_MATRIX.get(i, VERTEX);
+                nn.index = i;
+            }
+        }
 
         return nn;
     }
